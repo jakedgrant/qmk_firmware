6f806d55d798a8d3fa93a6e921b8a728bf1dc3b3
Jake G <jakegrantche@gmail.com>
2019-11-23 20:18:02 -0600

    Update keyboards/tokyo60/keymaps/jakedgrant/keymap.c

--- keyboards/tokyo60/keymaps/jakedgrant/keymap.c
+++ keyboards/tokyo60/keymaps/jakedgrant/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ├───────────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬───────────┬─────┤
 │   Shift   │  Z  │  X  │  C  │  V  │  B  │  N  │  M  │  ,  │  .  │  /  │   Shift   │ Fn  │
 ├─────┬─────┬────────┬───────────────────────────────────────────────┬────────┬─────┬─────┤
-│█████│ NU  │  Alt   │                     Space                     │  Alt   │ Gui │█████│
+│█████│ Gui │  Num   │                     Space                     │  Alt   │ Gui │█████│
 └─────┴─────┴────────┴───────────────────────────────────────────────┴────────┴─────┴─────┘
  */
 [BL] = LAYOUT_60_hhkb(
@@ -28,13 +28,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,    KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSPC,      \
     KC_LCTL,   KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,       KC_ENT,      \
     KC_LSFT,            KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M, KC_COMM,  KC_DOT, KC_SLSH,      KC_RSFT, MO(FN), \
-            MO(NU), KC_LALT,              KC_SPC,                         KC_RALT, KC_RGUI ),
+            KC_LGUI, MO(NU),              KC_SPC,                         KC_RALT, KC_RGUI ),
              
 /* 1: HHKB Fn layer
 ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
 │CtAlD│ F1  │ F2  │ F3  │ F4  │ F5  │ F6  │ F7  │ F8  │ F9  │ F10 │ F11 │ F12 │ Ins │ Del │
 ├───────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────────┤
-│ Caps  │ RGB │RGBfw│RGBrv│BLtog│BLstp│     │     │ Psc │ Slk │ Pus │ Up  |PrtSc|         │
+│ Caps  │ RGB │RGBfw│RGBrv│BLtog│BLstp│     │     │ Psc │ Slk │ Pus │ Up  |     |  PrtSc  │
 ├────────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬──────────────┤
 │        │ VoD │ VoU │ Mut │     │     │NP_* │NP_/ │Home │PgUp │Left │Right│    NPEnt     │
 ├───────────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬───────────┬─────┤
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [FN]= LAYOUT_60_hhkb(
     CtrlAltDel,  KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   KC_INS,  KC_DEL, \
-    KC_CAPS,  RGB_TOG, RGB_MOD, RGB_RMOD, BL_TOGG, BL_STEP, KC_TRNS, KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, KC_UP,   KC_PSCR,  KC_TRNS,  \
+    KC_CAPS,  RGB_TOG, RGB_MOD, RGB_RMOD, BL_TOGG, BL_STEP, KC_TRNS, KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, KC_UP,   KC_TRNS,  KC_PSCR,  \
     KC_TRNS,  KC_VOLD, KC_VOLU, KC_MUTE,  KC_TRNS, KC_TRNS, KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT,       KC_PENT,       \
     KC_TRNS,           KC_TRNS, KC_TRNS,  KC_CALC, KC_TRNS, KC_TRNS, KC_PPLS, KC_PMNS, KC_END,  KC_PGDN, KC_DOWN, KC_TRNS, KC_TRNS, \
             KC_TRNS, KC_TRNS,                   KC_TRNS,                         KC_TRNS, KC_TRNS ),
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ├───────────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬───────────┬─────┤
 │           │     │     │     │     │     │     │  1  │  2  │  3  │  .  │           │     │
 ├─────┬─────┬────────┬───────────────────────────────────────────────┬────────┬─────┬─────┤
-│█████│ *** │        │                           0                   │        │     │█████│
+│█████│     │   ***  │                           0                   │        │     │█████│
 └─────┴─────┴────────┴───────────────────────────────────────────────┴────────┴─────┴─────┘
  */
 [NU] = LAYOUT_60_hhkb(
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,     KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P7,   KC_P8,    KC_P9,    KC_PPLS, KC_NO, KC_NO, KC_BSPC,      \
     KC_NO,    KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P4,   KC_P5,    KC_P6,    KC_PPLS, KC_NO,       KC_PENT,      \
     KC_NO,              KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P1, KC_P2,  KC_P3, KC_PDOT,      KC_NO, KC_NO, \
-            KC_TRNS, KC_NO,             KC_P0,                          KC_NO, KC_NO )
+            KC_NO, KC_TRNS,             KC_P0,                          KC_NO, KC_NO )
 };
 
 /* Blank Layout
